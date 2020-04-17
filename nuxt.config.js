8436422d80c3699f9fe2068621cd7911fa747bb1
妹尾弦 <gengineer.web@gmail.com>
2020-04-17 16:30:04 +0900

    色の変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,8 +57,8 @@ export default {
     theme: {
       dark: false,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
+        light: {
+          primary: '#00acee',
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,
