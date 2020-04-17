b7eb89a919d2077246ea2000b30a6fc61ae3b93d
妹尾弦 <gengineer.web@gmail.com>
2020-04-17 16:29:52 +0900

    入力フォーム

--- pages/index.vue
+++ pages/index.vue
@@ -11,10 +11,69 @@
             COVID-19の早期収束を願います。
           </p>
           <hr class="my-3" />
+          <div>
+            <svg ref="svgArea" viewBox="0 0 191 100">
+              <rect
+                x="0"
+                y="0"
+                width="191"
+                height="100"
+                fill="#fff"
+                stroke="#12b886"
+                stroke-width="4"
+              ></rect>
+              <foreignObject
+                requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility"
+                width="188"
+                height="100"
+                x="4"
+                y="40"
+                font-size="12px"
+                style="line-height:1rem;"
+              >
+                {{ form1st.text }}
+              </foreignObject>
+            </svg>
+          </div>
+          <v-text-field
+            :counter="20"
+            v-model="form1st.text"
+            @click="initForm1st()"
+            label="共有してみよう！やりたいことその1"
+            color="primary"
+          >
+          </v-text-field>
+          <v-text-field
+            :counter="20"
+            v-model="form2nd.text"
+            @click="initForm2nd()"
+            label="やりたいことその2"
+            color="primary"
+          >
+          </v-text-field>
+          <v-text-field
+            :counter="20"
+            v-model="form3rd.text"
+            @click="initForm3rd()"
+            label="やりたいことその3"
+            color="primary"
+          >
+          </v-text-field>
         </v-card-text>
+
         <v-card-actions>
           <v-spacer />
-          <v-btn color="#00acee" nuxt to="/" large dark>
+          <v-btn color="primary" small dark>
+            <v-icon dark left>mdi-plus</v-icon>やりたいこと追加
+          </v-btn>
+          <v-btn color="primary" small dark>
+            <v-icon dark left>mdi-minus</v-icon>やりたいこと削除
+          </v-btn>
+          <v-spacer />
+        </v-card-actions>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" nuxt to="/inspire" large dark>
             <v-icon dark left>mdi-twitter</v-icon>Twitterでシェアする
           </v-btn>
           <v-spacer />
@@ -25,7 +84,44 @@
 </template>
 
 <script>
+const defaultMessage = '10万円どう使う？'
 export default {
-  components: {}
+  data() {
+    return {
+      formLength: 1,
+      form1st: {
+        init: false,
+        text: defaultMessage
+      },
+      form2nd: {
+        init: false,
+        text: defaultMessage
+      },
+      form3rd: {
+        init: false,
+        text: defaultMessage
+      }
+    }
+  },
+  methods: {
+    initForm1st() {
+      if (!this.form1st.init) {
+        this.form1st.text = ''
+        this.form1st.init = true
+      }
+    },
+    initForm2nd() {
+      if (!this.form2nd.init) {
+        this.form2nd.text = ''
+        this.form2nd.init = true
+      }
+    },
+    initForm3rd() {
+      if (!this.form3rd.init) {
+        this.form3rd.text = ''
+        this.form3rd.init = true
+      }
+    }
+  }
 }
 </script>
