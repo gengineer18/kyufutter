ad1bff047f72fcd4517c76293fba7e54a5006af0
妹尾弦 <gengineer.web@gmail.com>
2020-04-18 13:33:14 +0900

    firestoreとテスト投稿

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,35 @@
           </p>
           <hr class="my-3" />
           <div v-if="formLength == 1">
-            <svg-area-1 :text1st="form1st.text" />
+            <!-- <svg-area-1 ref="svgArea" :text1st="form1st.text" /> -->
+            <svg ref="svgArea" viewBox="0 0 191 100">
+              <rect
+                x="0"
+                y="0"
+                width="191"
+                height="100"
+                fill="#fefefe"
+                stroke="#FFB300"
+                stroke-width="4"
+              ></rect>
+              <text :height="height" width="100" x="4" y="12" font-size="8px">
+                10万円給付ったー
+              </text>
+              <foreignObject
+                :width="width"
+                :height="height"
+                :font-size="fontsize"
+                :style="style"
+                requiredFeatures="http://www.w3.org/TR/SVG11/feature#Extensibility"
+                x="4"
+                y="40"
+              >
+                {{ form1st.text }}
+              </foreignObject>
+              <text :height="height" width="100" x="125" y="90" font-size="8px">
+                #10万円こう使う
+              </text>
+            </svg>
           </div>
           <div v-if="formLength == 2">
             <svg-area-2 :text1st="form1st.text" :text2nd="form2nd.text" />
@@ -84,7 +112,7 @@
         </v-card-actions>
         <v-card-actions>
           <v-spacer />
-          <v-btn @click="submit()" color="warning" large dark>
+          <v-btn @click="create()" color="warning" large dark>
             <v-icon dark left>mdi-currency-jpy</v-icon>
             <strong>こう使う！！</strong>
           </v-btn>
@@ -111,13 +139,45 @@
 </template>
 
 <script>
-import SvgArea1 from '@/components/SvgArea1'
+// import SvgArea1 from '@/components/SvgArea1'
 import SvgArea2 from '@/components/SvgArea2'
 import SvgArea3 from '@/components/SvgArea3'
+import firebase from 'firebase'
+
 const defaultMessage = '10万円どう使う？'
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  storageBucket: process.env.STORAGE_BUCKET,
+  projectId: process.env.PROJECT_ID
+}
+if (!firebase.apps.length) {
+  firebase.initializeApp(config)
+}
+const db = firebase.firestore()
+const svg2imageData = (svgElement, successCallback, errorCallback) => {
+  const canvas = document.createElement('canvas')
+  canvas.width = 191
+  canvas.height = 100
+  const ctx = canvas.getContext('2d')
+  const image = new Image()
+  image.onload = () => {
+    ctx.drawImage(image, 0, 0, 191, 100)
+    successCallback(canvas.toDataURL())
+  }
+  image.onerror = (e) => {
+    errorCallback(e)
+  }
+  const svgData = new XMLSerializer().serializeToString(svgElement)
+  image.src =
+    'data:image/svg+xml;charset=utf-8;base64,' +
+    btoa(unescape(encodeURIComponent(svgData)))
+}
+
 export default {
   components: {
-    SvgArea1,
+    // SvgArea1,
     SvgArea2,
     SvgArea3
   },
@@ -141,7 +201,11 @@ export default {
         return value.length <= 15 || '15文字以内で入力してください'
       },
       counter: 15,
-      tweet: false
+      tweet: false,
+      width: 188,
+      height: 20,
+      fontsize: '12px',
+      style: 'style=line-height:12px;'
     }
   },
   methods: {
@@ -180,6 +244,31 @@ export default {
       if (this.$refs.form.validate()) {
         // すべてのバリデーションが通過したときのみ
         // if文の中に入る
+        console.log('true')
+        this.tweet = true
+      } else {
+        console.log('false')
+      }
+    },
+    create() {
+      if (this.$refs.form.validate()) {
+        svg2imageData(this.$refs.svgArea, async (data) => {
+          const storageRef = firebase.storage().ref()
+          const fileRef = storageRef.child(`test.png`)
+
+          // Firebase Cloud Storageにアップロード
+          await fileRef.putString(data, 'data_url')
+          const url = await fileRef.getDownloadURL()
+          console.log(url)
+
+          // Firestoreに保存しておく
+          const card = db.collection('cards').doc('test')
+          await card.set({
+            url,
+            message: this.form1st.text
+          })
+        })
+
         console.log('true')
         this.tweet = true
       } else {
--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,15 +1,99 @@
 <template>
-  <v-layout>
-    <v-flex class="text-center">
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-flex>
-  </v-layout>
+  <div class="hello">
+    <svg
+      ref="svgCard"
+      width="860px"
+      height="200px"
+      viewBox="0 0  860 520"
+      preserveAspectRatio="xMidYMid meet"
+    >
+      <rect
+        id="svgEditorBackground"
+        x="0"
+        y="0"
+        width="860"
+        height="200"
+        style="fill: none; stroke: none;"
+      />
+      <text
+        id="e1_texte"
+        style="fill:black;font-family:Arial;font-size:50px;"
+        x="56"
+        y="46"
+      >
+        {{ msg }}
+      </text>
+    </svg>
+    <input v-model="msg" type="text" />
+    <button @click="create">create</button>
+  </div>
 </template>
+
+<script>
+import firebase from 'firebase'
+
+// firebaseのconfig情報をペースト
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  storageBucket: process.env.STORAGE_BUCKET,
+  projectId: process.env.PROJECT_ID
+}
+if (!firebase.apps.length) {
+  firebase.initializeApp(config)
+}
+const db = firebase.firestore()
+
+// svgをpngに変換
+const svg2imageData = (svgElement, successCallback, errorCallback) => {
+  const canvas = document.createElement('canvas')
+  canvas.width = 1200
+  canvas.height = 630
+  const ctx = canvas.getContext('2d')
+  const image = new Image()
+  image.onload = () => {
+    ctx.drawImage(image, 0, 0, 1200, 630)
+    successCallback(canvas.toDataURL())
+  }
+  image.onerror = (e) => {
+    errorCallback(e)
+  }
+  const svgData = new XMLSerializer().serializeToString(svgElement)
+  image.src =
+    'data:image/svg+xml;charset=utf-8;base64,' +
+    btoa(unescape(encodeURIComponent(svgData)))
+}
+
+export default {
+  name: 'Hello',
+  data() {
+    return {
+      msg: 'Hello World',
+      uuid: '1', // 適当に採番する
+      description: 'Vue.jsとFirebaseでOGP生成アプリをつくってみます'
+    }
+  },
+  methods: {
+    create() {
+      // refでsvgCardをsvgに設定しているのでthis.$refs.svgCardで要素を取れます
+      svg2imageData(this.$refs.svgCard, async (data) => {
+        const sRef = firebase.storage().ref()
+        const fileRef = sRef.child(`${this.uuid}.png`)
+
+        // Firebase Cloud Storageにアップロード
+        await fileRef.putString(data, 'data_url')
+        const url = await fileRef.getDownloadURL()
+        console.log(url)
+
+        // Firestoreに保存しておく
+        const card = db.collection('cards').doc(this.uuid)
+        await card.set({
+          url,
+          message: this.description
+        })
+      })
+    }
+  }
+}
+</script>
